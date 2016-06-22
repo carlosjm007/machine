6971b3fd7454a2cb163286262caed288bed947c5
Carlos Manosalva <carlos.manosalva@unillanos.edu.co>
2016-06-21 19:04:04 -0500

    codigo reformado

--- Machine/carga_datos.py
+++ Machine/carga_datos.py
@@ -24,94 +24,92 @@ try:
 	# 	host='localhost',
 	# 	password='111111')
 	cur = con.cursor()
-	# for data in datos["jams"]:
-	# cur.execute("insert into ciudad (nombre, ciudad_id) values (%s,%s)",("Villavo","Colombi"))
-	# con.commit()
 	datos = traer_datos()
-	fecha = conv_to_date(datos["endTime"]) - timedelta(hours = 5)
+	print datos["endTimeMillis"], datos["startTimeMillis"], datos["startTime"], datos["endTime"]
+
+	endTimeMillis = datos["endTimeMillis"]
+	startTimeMillis = datos["startTimeMillis"]
+	startTime = conv_to_date(datos["startTime"])
+	endTime = conv_to_date(datos["endTime"])
+
 	for data in datos["jams"]:
-		delta = data["updateMillis"] - datos["startTimeMillis"]
-		actualizado =fecha + timedelta(milliseconds = delta)
-		try:
-			print actualizado, fecha, actualizado - fecha, data["street"].encode('utf8')
-		except:
-			pass
-		# cur.execute("select * from pais where nombre = '" + data["country"] + "'")
-		# if cur.fetchone() == None:
-		# 	cur.execute("select count(*) from pais")
-		# 	pais_id = cur.fetchone()[0]
-		# 	cur.execute("insert into pais (id, nombre) values (%s,%s)",((pais_id + 1),data["country"]))
-		# 	con.commit()
-		# 	print "no hay pais", pais_id
-		# else:
-		# 	cur.execute("select * from pais where nombre = '" + data["country"] + "'")
-		# 	pais_id = cur.fetchone()[0]
-		# 	print "si hay pais", pais_id
-
-		# cur.execute("select * from ciudad where nombre = '" + data["city"] + "' and  pais_id ='" + str(pais_id) + "'")
-		# if cur.fetchone() == None:
-		# 	cur.execute("select count(*) from ciudad")
-		# 	ciudad_id = cur.fetchone()[0]
-		# 	cur.execute("insert into ciudad (id, nombre,pais_id) values (%s,%s,%s)",((ciudad_id + 1),data["city"], str(pais_id)))
-		# 	con.commit()
-		# 	print "no hay ciudad", ciudad_id
-		# else:
-		# 	cur.execute("select * from ciudad where nombre = '" + data["city"] + "'")
-		# 	ciudad_id = cur.fetchone()[0]
-		# 	print "si hay ciudad", ciudad_id
-
-		# cur.execute("select * from lugar where nombre = '" + data["street"] + "' and  ciudad_id ='" + str(ciudad_id) + "'")
-		# if cur.fetchone() == None:
-		# 	cur.execute("select count(*) from lugar")
-		# 	lugar_id = cur.fetchone()[0]
-		# 	cur.execute("insert into lugar (id, nombre,ciudad_id) values (%s,%s,%s)",((lugar_id + 1),data["street"], str(ciudad_id)))
-		# 	con.commit()
-		# 	print "no hay lugar", lugar_id
-		# else:
-		# 	cur.execute("select * from lugar where nombre = '" + data["street"] + "'")
-		# 	lugar_id = cur.fetchone()[0]
-		# 	print "si hay lugar", lugar_id
-
-	'''
-	datos = traer_datos()
-	u = datetime.today()
-	for data in datos["jams"]:
-		try:
-			cur.execute("insert into pais (nombre) values ('" + data["country"] + "')")
+		cur.execute("select * from pais where nombre = '" + data["country"] + "'")
+		if cur.fetchone() == None:
+			cur.execute("select count(*) from pais")
+			pais_id = cur.fetchone()[0]
+			cur.execute("insert into pais (id, nombre) values (%s,%s)",((pais_id + 1),data["country"]))
 			con.commit()
-		except psycopg2.DatabaseError, e:
-			con.rollback()
-			print 'Error1 %s' % e 
+			print "no hay pais", pais_id
+		else:
+			cur.execute("select * from pais where nombre = '" + data["country"] + "'")
+			pais_id = cur.fetchone()[0]
+			print "si hay pais", pais_id
 
-		try:
-			cur.execute("insert into ciudad (nombre, pais_id) values ('" + data["city"] + "','" + data["country"] + "')")
+		cur.execute("select * from ciudad where nombre = '" + data["city"] + "' and  pais_id ='" + str(pais_id) + "'")
+		if cur.fetchone() == None:
+			cur.execute("select count(*) from ciudad")
+			ciudad_id = cur.fetchone()[0]
+			cur.execute("insert into ciudad (id, nombre,pais_id) values (%s,%s,%s)",((ciudad_id + 1),data["city"], str(pais_id)))
 			con.commit()
-		except psycopg2.DatabaseError, e:
-			con.rollback()
-			print 'Error2 %s' % e
+			print "no hay ciudad", ciudad_id
+		else:
+			cur.execute("select * from ciudad where nombre = '" + data["city"] + "'")
+			ciudad_id = cur.fetchone()[0]
+			print "si hay ciudad", ciudad_id
 
-		try:
-			cur.execute("insert into lugar (nombre, ciudad_id) values ('" + data["street"] + "','" + data["city"] + "')")
+		cur.execute("select * from lugar where nombre = '" + data["street"] + "' and  ciudad_id ='" + str(ciudad_id) + "'")
+		if cur.fetchone() == None:
+			cur.execute("select count(*) from lugar")
+			lugar_id = cur.fetchone()[0]
+			cur.execute("insert into lugar (id, nombre,ciudad_id) values (%s,%s,%s)",((lugar_id + 1),data["street"], str(ciudad_id)))
 			con.commit()
-		except psycopg2.DatabaseError, e:
-			con.rollback()
-			print 'Error3 %s' % e
+			print "no hay lugar", lugar_id
+		else:
+			cur.execute("select * from lugar where nombre = '" + data["street"] + "'")
+			lugar_id = cur.fetchone()[0]
+			print "si hay lugar", lugar_id
 
-		try:
-			cur.execute("select count(*) from velocidad")
-			numero = cur.fetchone()[0]
-			print numero, str(data["street"])
-			cur.execute("insert into velocidad (id, fecha, velocidad, lugar_id) values ('" + str(numero + 1) + "','" + u.strftime("%Y-%m-%d %H:%M:%S") + "','" + str(data["speed"]) + "','" + str(data["street"]) + "')")
+		# Guardar cada trancon
+
+	for data in datos["alerts"]:
+		cur.execute("select * from pais where nombre = '" + data["country"] + "'")
+		if cur.fetchone() == None:
+			cur.execute("select count(*) from pais")
+			pais_id = cur.fetchone()[0]
+			cur.execute("insert into pais (id, nombre) values (%s,%s)",((pais_id + 1),data["country"]))
 			con.commit()
-			for linea in data["line"]:
-				cur.execute("insert into coordenada (velocidad_id, latitud, longitud) values ('" + str(numero + 1) + "','" + str(linea["y"]) + "','" + str(linea["x"]) + "')")
-				con.commit()
-		except psycopg2.DatabaseError, e:
-			con.rollback()
-			print 'Error4 %s' % e
+			print "no hay pais", pais_id
+		else:
+			cur.execute("select * from pais where nombre = '" + data["country"] + "'")
+			pais_id = cur.fetchone()[0]
+			print "si hay pais", pais_id
 
-	print len(datos["jams"]),datos["jams"][0]["city"],datos["jams"][0]["country"]
-	'''
+		cur.execute("select * from ciudad where nombre = '" + data["city"] + "' and  pais_id ='" + str(pais_id) + "'")
+		if cur.fetchone() == None:
+			cur.execute("select count(*) from ciudad")
+			ciudad_id = cur.fetchone()[0]
+			cur.execute("insert into ciudad (id, nombre,pais_id) values (%s,%s,%s)",((ciudad_id + 1),data["city"], str(pais_id)))
+			con.commit()
+			print "no hay ciudad", ciudad_id
+		else:
+			cur.execute("select * from ciudad where nombre = '" + data["city"] + "'")
+			ciudad_id = cur.fetchone()[0]
+			print "si hay ciudad", ciudad_id
+
+		cur.execute("select * from lugar where nombre = '" + data["street"] + "' and  ciudad_id ='" + str(ciudad_id) + "'")
+		if cur.fetchone() == None:
+			cur.execute("select count(*) from lugar")
+			lugar_id = cur.fetchone()[0]
+			cur.execute("insert into lugar (id, nombre,ciudad_id) values (%s,%s,%s)",((lugar_id + 1),data["street"], str(ciudad_id)))
+			con.commit()
+			print "no hay lugar", lugar_id
+		else:
+			cur.execute("select * from lugar where nombre = '" + data["street"] + "'")
+			lugar_id = cur.fetchone()[0]
+			print "si hay lugar", lugar_id
+
+		# Guardar cada punto
+
 except psycopg2.DatabaseError, e:
 	print 'Error %s' % e
 	sys.exit(1)
